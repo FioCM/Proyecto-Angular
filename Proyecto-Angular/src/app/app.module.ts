d1bf6c4ae4a8b729cb2d0460b6b75ac040f66de6
Ashley Castillo <afiocastimo02@gmail.com>
2022-06-13 17:00:44 -0600

    Services-Screen
    I created the Services-Screen and stared the code

--- Proyecto-Angular/src/app/app.module.ts
+++ Proyecto-Angular/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { CardsComponent } from './cards/cards.component';
 import { TypesComponent } from './types/types.component';
+import { ServicesComponent } from './services/services.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { TypesComponent } from './types/types.component';
     NavBarComponent,
     HomeComponent,
     CardsComponent,
-    TypesComponent
+    TypesComponent,
+    ServicesComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
