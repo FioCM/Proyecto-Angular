5d39e79c288c54aee018fdc87d5f2fdd467e968e
Ashley Castillo <afiocastimo02@gmail.com>
2022-06-14 02:07:55 -0600

    Contact and Error Page
    I finished the Contact and ErrorPage Screens

--- Proyecto-Angular/src/app/app-routing.module.ts
+++ Proyecto-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { TypesComponent } from './types/types.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "types", component: TypesComponent},
+  {path: "services", component: ServicesComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "", component: HomeComponent},
+  {path: "**", component: ErrorPageComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
